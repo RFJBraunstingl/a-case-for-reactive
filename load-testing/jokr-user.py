af58fb87331b7eba90860165dd58156fd74e0b42
RFJBraunstingl <rfj.braunstingl@gmail.com>
2021-05-09 18:22:00 +0200

    add load testing

---
+++ load-testing/jokr-user.py
@@ -0,0 +1,13 @@
+from locust import HttpUser, task, between
+
+
+class QuickstartUser(HttpUser):
+    wait_time = between(0.2, 0.3)
+
+    @task(10)
+    def read_random_joke(self):
+        self.client.get("/api/joke")
+
+    @task
+    def write_new_joke(self):
+        self.client.post("/api/joke", "a new joke")
