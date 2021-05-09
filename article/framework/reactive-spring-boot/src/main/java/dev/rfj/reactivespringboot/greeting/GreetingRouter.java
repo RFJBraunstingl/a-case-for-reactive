bbe5335827aa47cc9d272e15dfba3200fd6c7874
RFJBraunstingl <rfj.braunstingl@gmail.com>
2021-05-09 09:11:52 +0200

    add example reactive implementation

---
+++ article/framework/reactive-spring-boot/src/main/java/dev/rfj/reactivespringboot/greeting/GreetingRouter.java
@@ -0,0 +1,23 @@
+package dev.rfj.reactivespringboot.greeting;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.http.MediaType.TEXT_PLAIN;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+
+@Configuration
+public class GreetingRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> route(GreetingHandler handler) {
+        return RouterFunctions.route(
+                GET("/hello").and(accept(TEXT_PLAIN)),
+                handler::sayHello
+        );
+    }
+}
